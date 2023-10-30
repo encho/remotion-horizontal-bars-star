75347ff888d8df9c75cdc1a54535d91b16aba13a
Lorenzo Bertolini <ciao@lorenzobertolini.com>
2023-10-30 10:45:16 +0100

    use bundesliga api to display actual table

--- src/Root.tsx
+++ src/Root.tsx
@@ -4,15 +4,12 @@ import {
 	HorizontalBarsStar,
 	horizontalBarsStarSchema,
 } from './HorizontalBarsStar/HorizontalBarsStar';
-
-import bundesligaBarChart from './data/bundesliga/bundesliga';
+import {
+	BundesligaTop10BarChart,
+	bundesligaTop10BarChartSchema,
+} from './BundesligaTop10BarChart/BundesligaTop10BarChart';
 
 import './style.css';
-
-// const horizontalVideo = {
-// 	width: 1920,
-// 	height: 1080,
-// };
 
 const squareVideo = {
 	width: 1080,
@@ -29,13 +26,30 @@ export const RemotionRoot: React.FC = () => {
 				fps={30}
 				{...squareVideo}
 				schema={horizontalBarsStarSchema}
+				defaultProps={barChartFixtureProps}
+			/>
+			<Composition
+				id="BundesligaTop10BarChart"
+				component={BundesligaTop10BarChart}
+				durationInFrames={240}
+				fps={30}
+				{...squareVideo}
+				schema={bundesligaTop10BarChartSchema}
 				defaultProps={{
-					titleText: `Top 10 Bundesliga Teams - ${bundesligaBarChart.dateLabel}`,
-					titleFontSize: 44,
-					titleColor: bundesligaBarChart.colors.title,
-					backgroundColor: bundesligaBarChart.colors.background,
-					progressColor: bundesligaBarChart.colors.progress,
-					items: bundesligaBarChart.items,
+					dateString: '29. Oktober 2023',
+					year: 2023,
+					apiData: null,
+				}}
+				calculateMetadata={async ({props}) => {
+					const apiUrl = `https://api.openligadb.de/getbltable/bl1/${props.year}`;
+					const data = await fetch(apiUrl);
+					const json = await data.json();
+					return {
+						props: {
+							...props,
+							apiData: json,
+						},
+					};
 				}}
 			/>
 			<Composition
@@ -54,4 +68,86 @@ export const RemotionRoot: React.FC = () => {
 			/>
 		</>
 	);
+};
+
+const barChartFixtureProps = {
+	titleText: 'Top 10 Bundesliga Teams | 29. Oktober 2023',
+	titleFontSize: 44,
+	titleColor: '#CAD8DE',
+	backgroundColor: '#0b132b',
+	progressColor: '#CAD8DE',
+	items: [
+		{
+			label: 'Bayer Leverkusen',
+			description: '25 Punkte',
+			value: 25,
+			colors: {background: '#1c2541', text: '#CAD8DE', border: '#76E7CD'},
+			imageSource:
+				'https://upload.wikimedia.org/wikipedia/de/thumb/f/f7/Bayer_Leverkusen_Logo.svg/1200px-Bayer_Leverkusen_Logo.svg.png',
+		},
+		{
+			label: 'FC Bayern München',
+			description: '23 Punkte',
+			value: 23,
+			colors: {background: '#1c2541', text: '#CAD8DE', border: '#76E7CD'},
+			imageSource: 'https://i.imgur.com/jJEsJrj.png',
+		},
+		{
+			label: 'VfB Stuttgart',
+			description: '21 Punkte',
+			value: 21,
+			colors: {background: '#1c2541', text: '#CAD8DE', border: '#76E7CD'},
+			imageSource: 'https://i.imgur.com/v0tkpNx.png',
+		},
+		{
+			label: 'Borussia Dortmund',
+			description: '21 Punkte',
+			value: 21,
+			colors: {background: '#1c2541', text: '#CAD8DE', border: '#76E7CD'},
+			imageSource:
+				'https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/Borussia_Dortmund_logo.svg/560px-Borussia_Dortmund_logo.svg.png',
+		},
+		{
+			label: 'RB Leipzig',
+			description: '20 Punkte',
+			value: 20,
+			colors: {background: '#1c2541', text: '#CAD8DE', border: '#9B7EDE'},
+			imageSource: 'https://i.imgur.com/Rpwsjz1.png',
+		},
+		{
+			label: 'TSG 1899 Hoffenheim',
+			description: '18 Punkte',
+			value: 18,
+			colors: {background: '#1c2541', text: '#CAD8DE', border: '#C45BAA'},
+			imageSource: 'https://i.imgur.com/gF0PfEl.png',
+		},
+		{
+			label: 'Eintracht Frankfurt',
+			description: '14 Punkte',
+			value: 14,
+			colors: {background: '#1c2541', text: '#CAD8DE', border: 'transparent'},
+			imageSource: 'https://i.imgur.com/X8NFkOb.png',
+		},
+		{
+			label: 'SC Freiburg',
+			description: '13 Punkte',
+			value: 13,
+			colors: {background: '#1c2541', text: '#CAD8DE', border: 'transparent'},
+			imageSource: 'https://i.imgur.com/r3mvi0h.png',
+		},
+		{
+			label: 'VfL Wolfsburg',
+			description: '12 Punkte',
+			value: 12,
+			colors: {background: '#1c2541', text: '#CAD8DE', border: 'transparent'},
+			imageSource: 'https://i.imgur.com/ucqKV4B.png',
+		},
+		{
+			label: 'FC Augsburg',
+			description: '11 Punkte',
+			value: 11,
+			colors: {background: '#1c2541', text: '#CAD8DE', border: 'transparent'},
+			imageSource: 'https://i.imgur.com/sdE62e2.png',
+		},
+	],
 };
